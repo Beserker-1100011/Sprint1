dd7617297a4480eaf2f0e2599b04607240a29a10
Bhaskar <58306840+Beserker-1100011@users.noreply.github.com>
2021-07-05 16:01:36 +0530

    Add files via upload

---
+++ Payload.java
@@ -0,0 +1,16 @@
+package com.abc.techstore.payload;
+
+
+public class Payload {
+	private int productId;
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	
+
+}
